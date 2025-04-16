4206d737f37fe838e8a70f9d87256a28e3dbea85
Tegar Aditya <tegaraditya53@gmail.com>
2025-04-16 11:39:35 +0700

    add AuthorizeIP middleware to restrict access by IP address

--- internal/api/middlewares/middleware.go
+++ internal/api/middlewares/middleware.go
@@ -1,1 +1,17 @@
 package middlewares
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// AuthorizeIP is a middleware that checks if the request comes from an authorized IP address.
+func AuthorizeIP() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		if c.IP() != "127.0.0.1" {
+			return c.Status(fiber.StatusForbidden).SendString(fmt.Sprintf("IP %s is not authorized", c.IP()))
+		}
+		return c.Next()
+	}
+}
--- internal/api/routes/route.go
+++ internal/api/routes/route.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/tegaraditya/mmj-whatsapp-client/internal/api/handlers"
+	"github.com/tegaraditya/mmj-whatsapp-client/internal/api/middlewares"
 	"github.com/tegaraditya/mmj-whatsapp-client/pkg/whatsapp"
 )
 
@@ -10,5 +11,5 @@ func SetupRoutes(app *fiber.App, wac *whatsapp.WhatsAppClient) {
 	h := handlers.CreateHandler(wac)
 
 	app.Get("/", h.GetAppInfo)
-	app.Post("/send", h.SendMessage)
+	app.Post("/send", middlewares.AuthorizeIP(), h.SendMessage)
 }

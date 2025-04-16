51a36655b63e8226ae36c8a84788c8c4c345d35f
Tegar Aditya <tegaraditya53@gmail.com>
2025-04-16 11:39:28 +0700

    load environment variables and add logger middleware

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -12,11 +12,19 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/healthcheck"
 	"github.com/gofiber/fiber/v2/middleware/helmet"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
 	"github.com/tegaraditya/mmj-whatsapp-client/internal/api/routes"
 	"github.com/tegaraditya/mmj-whatsapp-client/pkg/whatsapp"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("Failed to load .env file: %v", err)
+		fmt.Println("Using default environment variables")
+	}
+
 	app := fiber.New(fiber.Config{
 		JSONEncoder:  sonic.Marshal,
 		JSONDecoder:  sonic.Unmarshal,
@@ -26,6 +34,7 @@ func main() {
 	app.Use(cors.New())
 	app.Use(helmet.New())
 	app.Use(healthcheck.New())
+	app.Use(logger.New())
 
 	client, err := whatsapp.NewClient()
 	if err != nil {
@@ -39,7 +48,7 @@ func main() {
 
 	routes.SetupRoutes(app, client)
 
-	log.Fatal(app.Listen(fmt.Sprintf(":%d", 3000)))
+	log.Fatal(app.Listen(fmt.Sprintf(":%s", "3000")))
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)

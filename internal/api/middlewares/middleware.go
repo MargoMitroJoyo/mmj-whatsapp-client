b366947375c886fd947e1c2ff1de91b328bfb78f
Tegar Aditya <tegaraditya53@gmail.com>
2025-04-17 14:03:12 +0700

    refactor: enhance AuthorizeIP middleware to support additional authorized IP ranges

--- internal/api/middlewares/middleware.go
+++ internal/api/middlewares/middleware.go
@@ -2,6 +2,7 @@ package middlewares
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -9,8 +10,9 @@ import (
 // AuthorizeIP is a middleware that checks if the request comes from an authorized IP address.
 func AuthorizeIP() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		if c.IP() != "127.0.0.1" {
-			return c.Status(fiber.StatusForbidden).SendString(fmt.Sprintf("IP %s is not authorized", c.IP()))
+		ip := c.IP()
+		if ip != "127.0.0.1" && !strings.HasPrefix(ip, "192.168.") && !strings.HasPrefix(ip, "172.") {
+			return c.Status(fiber.StatusForbidden).SendString(fmt.Sprintf("IP %s is not authorized", ip))
 		}
 		return c.Next()
 	}

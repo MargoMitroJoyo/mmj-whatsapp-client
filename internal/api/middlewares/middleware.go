66cf0c7cc2e36b8ea0fce5d4e9ebd0067db57dde
Tegar Aditya <tegaraditya53@gmail.com>
2025-04-18 22:56:03 +0700

    refactor: improve AuthorizeIP middleware to validate IP addresses using CIDR blocks

--- internal/api/middlewares/middleware.go
+++ internal/api/middlewares/middleware.go
@@ -2,17 +2,41 @@ package middlewares
 
 import (
 	"fmt"
-	"strings"
+	"net"
 
 	"github.com/gofiber/fiber/v2"
 )
 
+var privateIPBlocks []*net.IPNet
+
+func init() {
+	for _, cidr := range []string{
+		"127.0.0.0/8",    // loopback
+		"10.0.0.0/8",     // class A
+		"172.16.0.0/12",  // class B
+		"192.168.0.0/16", // class C
+	} {
+		_, block, _ := net.ParseCIDR(cidr)
+		privateIPBlocks = append(privateIPBlocks, block)
+	}
+}
+
+// isPrivateIP checks if the given IP is in a private range
+func isPrivateIP(ip net.IP) bool {
+	for _, block := range privateIPBlocks {
+		if block.Contains(ip) {
+			return true
+		}
+	}
+	return false
+}
+
 // AuthorizeIP is a middleware that checks if the request comes from an authorized IP address.
 func AuthorizeIP() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		ip := c.IP()
-		if ip != "127.0.0.1" && !strings.HasPrefix(ip, "192.168.") && !strings.HasPrefix(ip, "172.") {
-			return c.Status(fiber.StatusForbidden).SendString(fmt.Sprintf("IP %s is not authorized", ip))
+		ip := net.ParseIP(c.IP())
+		if ip == nil || !isPrivateIP(ip) {
+			return c.Status(fiber.StatusForbidden).SendString(fmt.Sprintf("IP %s is not authorized", c.IP()))
 		}
 		return c.Next()
 	}

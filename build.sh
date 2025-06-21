fa0674a8248b6230383f0624e017a46742ac7ec9
Tegar Aditya <tegaraditya53@gmail.com>
2025-06-21 13:02:12 +0700

    refactor: update build script to use POSIX compliant shell

--- build.sh
+++ build.sh
@@ -1,22 +1,26 @@
-#!/bin/bash
+#!/bin/sh
 
 # Set output directory
 OUTPUT_DIR="./build/bin"
 OUTPUT_FILE="$OUTPUT_DIR/main"
 
 # Ensure the output directory exists
+# 'mkdir -p' is supported by busybox (Alpine's toolset)
 mkdir -p "$OUTPUT_DIR"
 
 # Remove the existing build file if it exists
+# '[ -f ... ]' is a standard POSIX test command
 if [ -f "$OUTPUT_FILE" ]; then
     rm "$OUTPUT_FILE"
     echo "Removed existing build file: $OUTPUT_FILE"
 fi
 
 # Build the Go application for Linux
-GOOS=linux GOARCH=amd64 CGO_ENABLED=1 go build -ldflags="-s -w" -o "$OUTPUT_FILE" ./cmd/app/main.go
+echo "Building Go application for Linux..."
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags="-s -w" -o "$OUTPUT_FILE" ./cmd/app/main.go
 
 # Check if the build was successful
+# '$?' is the standard way to get the exit code in any POSIX shell
 if [ $? -ne 0 ]; then
     echo "Go build failed. Exiting."
     exit 1
